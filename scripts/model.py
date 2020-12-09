519e8749ecc0181a319b0e202bd7bd9d7c88f450
Loic Coyle <loic.coyle@hotmail.fr>
2020-12-09 22:58:45 +0100

    update model metadata saving

--- scripts/model.py
+++ scripts/model.py
@@ -2,7 +2,8 @@ import json
 from pathlib import Path
 
 import pandas as pd
-from pyobsbox import ObsBoxGenerator, make_lstm_ae, make_conv_ae
+from pyobsbox import ObsBoxGenerator
+from pyobsbox.models import make_lstm_ae, make_conv_ae
 from pyobsbox.utils import df_to_name
 
 from paths import metadata_folder, models_folder
@@ -18,7 +19,11 @@ if __name__ == "__main__":
     train_metadata = metadata_df.drop(validation_metadata.index)
 
     generator = ObsBoxGenerator(
-        train_metadata, metadata_path=metadata_path, n_bunches=128, sequence_length=2048
+        train_metadata,
+        metadata_path=metadata_path,
+        n_bunches=512,
+        sequence_length=2048,
+        shuffle=True,
     )
     validation_generator = ObsBoxGenerator(
         validation_metadata,
@@ -30,15 +35,27 @@ if __name__ == "__main__":
     model.compile(loss="mse", optimizer="adam")
     model.summary()
     # train
-    hist = model.fit(generator, validation_data=validation_generator, epochs=5)
+    hist = model.fit(
+        generator,
+        steps_per_epoch=10,
+        validation_data=validation_generator,
+        validation_steps=2,
+        epochs=25,
+        use_multiprocessing=False,
+        workers=8,
+    )
     # Save the model to file
     model_name = df_to_name(metadata_df, base="model", extension="")
     model_path = models_folder / model_name
     while model_path.is_dir():
         model_path = models_folder / (model_path.name + "_")
     model.save(model_path)
-    json.dump(hist.history, (model_path / "history.json").open("w"))
-    json.dump(generator.to_dict(), (model_path / "generator.json").open("w"))
-    json.dump(train_metadata.index.to_list(), (model_path / "train_index.json").open("w"))
-    json.dump(validation_metadata.index.to_list(), (model_path / "validation_index.json").open("w"))
-    json.dump(generator.to_dict(), (model_path / "generator.json").open("w"))
+    # save model metadata
+    model_metadata = {}
+    model_metadata["generator"] = generator.to_dict()
+    model_metadata["history"] = hist.history
+    model_metadata["train_indices"] = train_metadata.index.to_list()
+    model_metadata["validation_indices"] = validation_metadata.index.to_list()
+    json.dump(model_metadata, (model_path / "metadata.json").open("w"))
+    generator.fetched.to_hdf(model_path / "seen_train_metadata.h5", key='metadata')
+    validation_generator.fetched.to_hdf(model_path / "seen_validation_metadata.h5", key="metadata")
